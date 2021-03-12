90ee3716d0620483e277743b08e991d2794ec503
Thomas Strub <thomstrub@gmail.com>
2021-03-12 14:35:00 -0800

    adds a few tests

---
+++ intro-typescript.js
@@ -0,0 +1,6 @@
+var message = "Hello World";
+console.log(message);
+var person = {
+    name: 'Alex',
+    age: 32
+};
---
+++ intro-typescript.ts
@@ -0,0 +1,10 @@
+const message: string = "Hello World"
+console.log(message)
+
+const person: {
+    name: string,
+    age: number
+} = {
+    name: 'Alex',
+    age: 32
+}
